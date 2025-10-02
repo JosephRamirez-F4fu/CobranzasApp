bb7e2870a110a8a21bed1bff3c9e4e97c96e2784
Joseph Ramirez Sarmiento <U20211C828@upc.edu.pe>
2025-10-02 15:23:08 -0500

    feat(institution): add student management module

---
+++ src/app/institution/components/student-form/student-form.component.ts
@@ -0,0 +1,37 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-institution-student-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './student-form.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InstitutionStudentFormComponent {
+  @Input({ required: true }) form!: FormGroup;
+  @Input() saving = false;
+  @Input() editing = false;
+  @Output() submitted = new EventEmitter<void>();
+  @Output() canceled = new EventEmitter<void>();
+  @Output() cleared = new EventEmitter<void>();
+
+  onSubmit() {
+    this.submitted.emit();
+  }
+
+  onCancel() {
+    this.canceled.emit();
+  }
+
+  onClear() {
+    this.cleared.emit();
+  }
+}
