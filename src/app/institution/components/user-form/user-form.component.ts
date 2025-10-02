415450aced66a4f2325453d4bd62496f1292f875
Joseph Ramirez Sarmiento <U20211C828@upc.edu.pe>
2025-10-02 13:53:10 -0500

    Refine institution user management styling with Tailwind

---
+++ src/app/institution/components/user-form/user-form.component.ts
@@ -0,0 +1,32 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-institution-user-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './user-form.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InstitutionUserFormComponent {
+  @Input({ required: true }) form!: FormGroup;
+  @Input() saving = false;
+  @Input() editing = false;
+  @Output() submitted = new EventEmitter<void>();
+  @Output() canceled = new EventEmitter<void>();
+
+  onSubmit() {
+    this.submitted.emit();
+  }
+
+  onCancel() {
+    this.canceled.emit();
+  }
+}
